69bab2e54f2df4e0c0da97e7548349c73dafa3de
glebk <glebkot57@gmail.com>
2023-05-31 20:18:28 +0200

    1

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,11 +16,22 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
-        self.price = price
+        self.__name = name
+        self.price = float(price)
         self.quantity = int(quantity)
         Item.all.append(self)
 
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name = value
+        if len(value) <= 10:
+            return value
+        else:
+            print("наименованиe товара больше 10 симвовов")
 
     def calculate_total_price(self) -> float:
         """
@@ -36,3 +50,27 @@ class Item:
         self.price *= self.pay_rate
         return self.price
 
+    @classmethod
+    def instantiate_from_csv(cls):
+
+        file = "..\src\items.csv"
+
+        with open(file, "r", encoding="cp1251") as file:
+            reader = csv.DictReader(file)
+
+            for row in reader:
+                name = row["name"]
+                price = float(row["price"])
+                quantity = int(row["quantity"])
+                item = cls(name, price, quantity)
+                cls.all.append(item)
+
+        return cls.all
+
+    @staticmethod
+    def string_to_number(value):
+        try:
+            return float(value), int(value)
+        except ValueError:
+            print("Не преобразовать в строку")
+            return None
