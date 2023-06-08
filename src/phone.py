6377fc0f6ec690cc0cc23914b631591f8052ec1a
glebk <glebkot57@gmail.com>
2023-06-08 16:09:02 +0200

    add decorator number_of_sim

--- src/phone.py
+++ src/phone.py
@@ -15,9 +15,20 @@ class Phone(Item):
     def number_of_sim(self, value):
         self._number_of_sim = validate_number_of_sim(value)
 
+    @property
+    def number_of_sim(self):
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, value):
+        if value <= 0:
+            raise ValueError("The number of physical SIM cards must be an integer greater than zero.")
+        self.__number_of_sim = value
+
     def __add__(self, other):
         if issubclass(other.__class__, self.__class__):
             return self.quantity + other.quantity
+        raise Exception
 
     def __repr__(self):
         return f"Phone('{self.name}', {int(self.price)}, {self.quantity}, {self.number_of_sim})"
