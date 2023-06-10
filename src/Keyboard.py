70506181a718728c23a4bb3f8754a07a611a9c30
glebk <glebkot57@gmail.com>
2023-06-10 20:55:22 +0200

    add new class Keyboard and property

--- src/Keyboard.py
+++ src/Keyboard.py
@@ -7,10 +7,10 @@ class Keyboard(Item):
         super().__init__(name, price, quantity)
         self.language = "EN"
 
-    def change_lang(self, new_lang):
-        if new_lang in ["EN", "RU"]:
-            self.language = new_lang
-            print(f"The keyboard language has been changed to {new_lang}")
+    def change_lang(self, language):
+        if language in ["EN", "RU"]:
+            self.language = language
+            print(f"The keyboard language has been changed to {language}")
         else:
             print("Invalid language. Only EN and RU languages are supported.")
 
@@ -23,9 +23,4 @@ class Keyboard(Item):
         if new_lang in ["EN", "RU"]:
             self._language = new_lang
         else:
-            raise AttributeError("Неверный язык. Поддерживаются только EN и RU.")
-
-"""
-Разобраться с Миксингом
- 
- """
+            raise AttributeError("")
