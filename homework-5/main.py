9238843b484a3b4836f7d45fa6710010e2eae483
glebk <glebkot57@gmail.com>
2023-06-12 18:43:36 +0200

    add new class Keyboard and property

--- homework-5/main.py
+++ homework-5/main.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     assert str(kb.language) == "RU"
 
     # Сделали RU -> EN -> RU
-    kb.change_lang().change_lang()
+    kb.change_lang()
     assert str(kb.language) == "RU"
 
     kb.language = 'CH'
